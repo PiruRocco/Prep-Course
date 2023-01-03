9de56ed7970575fd8f1e17e8b4ec06f7baf85fbb
Lucas Perucho <perucholucas02@gmail.com>
2023-01-02 21:14:47 -0300

    homework 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -246,15 +246,16 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+      
+  if(letra.length > 1){
+    return "Dato incorrecto"
+  }
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
 
-    if (letra == a, e, i, o, u) {
-      return "es vocal"
-    };
-    if else { 
-      return "dato incorrecto"
-    }
-     
-  
+    
 }
 
 
